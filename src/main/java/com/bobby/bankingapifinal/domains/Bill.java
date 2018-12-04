6597e28e3df5bbbe80130f89ea8085b476193565
DerianW <ScratchGobo@gmail.com>
2018-12-04 15:42:11 -0500

    Fixed typos... and added @Entity lol

--- src/main/java/com/bobby/bankingapifinal/domains/Bill.java
+++ src/main/java/com/bobby/bankingapifinal/domains/Bill.java
@@ -3,11 +3,13 @@ package com.bobby.bankingapifinal.domains;
 
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 
 
+@Entity
 public class Bill
 {
 
