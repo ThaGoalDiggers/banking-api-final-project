af3115d757e170a8e21f8cd89f99e4c63ed2ab36
DerianW <ScratchGobo@gmail.com>
2018-12-04 15:35:29 -0500

    Derian: added a method in bill controller

--- src/main/java/com/bobby/bankingapifinal/controllers/BillController.java
+++ src/main/java/com/bobby/bankingapifinal/controllers/BillController.java
@@ -7,6 +7,7 @@ import com.bobby.bankingapifinal.services.BillService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -23,7 +24,7 @@ public class BillController
 
 
 
-    @RequestMapping("/accounts/{accountId}/bills")
+    @RequestMapping(method = RequestMethod.GET, value = "/accounts/{accountId}/bills")
     public List<Bill> getAllBillsByAccount(@PathVariable Long id)
     { return billService.getAllBillsByAccount(id); }
 
