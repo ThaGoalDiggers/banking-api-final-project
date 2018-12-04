ccc8acfb20c60df1e852d69f38a2213efe6f1030
BebopBobby <bbbgalvin4@gmail.com>
2018-12-04 09:45:33 -0500

    master

---
+++ src/main/java/com/bobby/bankingapifinal/BankingApiFinalApplication.java
@@ -0,0 +1,12 @@
+package com.bobby.bankingapifinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingApiFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingApiFinalApplication.class, args);
+	}
+}
