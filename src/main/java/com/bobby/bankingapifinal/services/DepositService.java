1c5a2e4e187568f9c1cee5aab3833bf17462019a
BebopBobby <bbbgalvin4@gmail.com>
2018-12-10 14:23:06 -0500

    implemented uml of withdrawal and deposit

--- src/main/java/com/bobby/bankingapifinal/services/DepositService.java
+++ src/main/java/com/bobby/bankingapifinal/services/DepositService.java
@@ -1,10 +1,13 @@
 package com.bobby.bankingapifinal.services;
 
+import com.bobby.bankingapifinal.domains.Account;
 import com.bobby.bankingapifinal.domains.Deposit;
 import com.bobby.bankingapifinal.repositories.DepositRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,19 +20,28 @@ public class DepositService {
     private AccountService accountService;
 
     public Iterable<Deposit> getAllDeposits(){
-        return depositRepository.findAll();
+       return depositRepository.findAll();
     }
 
     public Optional<Deposit> getDepositById(Long id){
         return depositRepository.findById(id);
     }
 
-    public void createDeposit(Deposit deposit){
-        depositRepository.save(deposit);
+    public void createDeposit(Deposit deposit, Long accountId){
+        for (Account acc : accountService.getAllAccounts()){
+            if (acc.getId().equals(accountId)){
+                deposit.setAccount(acc);
+                depositRepository.save(deposit);
+            }
+        }
     }
 
-    public void updateDeposit(Deposit deposit){
-        depositRepository.save(deposit);
+    public void updateDeposit(Deposit deposit, Long depositId){
+        for (Deposit d : depositRepository.findAll()){
+            if (d.getId().equals(depositId)){
+                depositRepository.save(deposit);
+            }
+        }
     }
 
     public void deleteDeposit(Long id){
--- src/main/java/com/bobby/bankingapifinal/services/WithdrawalService.java
+++ src/main/java/com/bobby/bankingapifinal/services/WithdrawalService.java
@@ -1,10 +1,13 @@
 package com.bobby.bankingapifinal.services;
 
+import com.bobby.bankingapifinal.domains.Account;
 import com.bobby.bankingapifinal.domains.Withdrawal;
 import com.bobby.bankingapifinal.repositories.WithdrawalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -24,12 +27,21 @@ public class WithdrawalService {
         return withdrawalRepository.findById(id);
     }
 
-    public void createWithdrawal(Withdrawal withdrawal){
-        withdrawalRepository.save(withdrawal);
+    public void createWithdrawal(Withdrawal withdrawal, Long accountId){
+        for (Account acc : accountService.getAllAccounts()){
+            if (acc.getId().equals(accountId)){
+                withdrawal.setAccount(acc);
+                withdrawalRepository.save(withdrawal);
+            }
+        }
     }
 
-    public void updateWithdrawal(Withdrawal withdrawal){
-        withdrawalRepository.save(withdrawal);
+    public void updateWithdrawal(Withdrawal withdrawal, Long withdrawalId){
+        for (Withdrawal w : withdrawalRepository.findAll()){
+            if (w.getId().equals(withdrawalId)){
+                withdrawalRepository.save(withdrawal);
+            }
+        }
     }
 
     public void deleteDeposit(Long id){
