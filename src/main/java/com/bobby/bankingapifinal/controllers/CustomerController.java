12acdeae8fe9af4b779b5834ee76688ce5563b0a
DerianW <ScratchGobo@gmail.com>
2018-12-11 22:05:40 -0500

    added cross origin notation

--- src/main/java/com/bobby/bankingapifinal/controllers/CustomerController.java
+++ src/main/java/com/bobby/bankingapifinal/controllers/CustomerController.java
@@ -14,6 +14,7 @@ import java.net.URI;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class CustomerController {
 
     @Autowired
--- src/main/java/com/bobby/bankingapifinal/controllers/DepositController.java
+++ src/main/java/com/bobby/bankingapifinal/controllers/DepositController.java
@@ -14,6 +14,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class DepositController {
     @Autowired
     private DepositService depositService;
